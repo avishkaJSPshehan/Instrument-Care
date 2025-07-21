4ad99ffe4f621bc2cbc1b95a805a808fce9fc175
Avishka Shehan <152954657+avishkaJSPshehan@users.noreply.github.com>
2025-07-21 13:58:33 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home Page</title>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+</head>
+<body>
+
+<?php include 'app/components/navbar.php'; ?>
+</body>
+</html>
