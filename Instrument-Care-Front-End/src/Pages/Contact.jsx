7961256d2eff50f07f047012519ec6f299ec9b45
Avishka Shehan <152954657+avishkaJSPshehan@users.noreply.github.com>
2025-07-24 12:53:40 +0530

    Create and style about page hero section

--- Instrument-Care-Front-End/src/Pages/Contact.jsx
+++ Instrument-Care-Front-End/src/Pages/Contact.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from '../Components/Common/Navbar'
+import Footer from '../Components/Common/Footer'
 
 export default function Contact() {
   return (
@@ -8,6 +9,7 @@ export default function Contact() {
       <div className="p-6">
         <h1 className="text-3xl font-bold">Contact Page</h1>
       </div>
+      <Footer />
     </>
   )
 }
