7e6888ecc4fb2d82f9a6572e54b591633ad67581
Avishka Shehan <152954657+avishkaJSPshehan@users.noreply.github.com>
2025-07-29 14:10:42 +0530

    Styling and done Login Page

--- Instrument-Care-Front-End/src/Pages/Auth/login.jsx
+++ Instrument-Care-Front-End/src/Pages/Auth/login.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Navbar from '../../Components/Common/Navbar'
 import LoginPage from '../../Components/auth/Login'
+import Footer from '../../Components/Common/Footer'
 
 export default function Login() {
   return (
