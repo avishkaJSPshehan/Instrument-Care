36d16c6f273080df3667ab795f8ad2acbf956b88
JAYASIRI S.P.A.S. <ict21860@fot.sjp.ac.lk>
2025-07-31 22:31:42 +0530

    Make login screen more responsive

--- Instrument-Care-Front-End/src/Components/Common/Navbar.jsx
+++ Instrument-Care-Front-End/src/Components/Common/Navbar.jsx
@@ -10,9 +10,9 @@ export default function Navbar() {
   const navLinks = ["Home", "About", "Technician", "Contact"];
 
   return (
-    <nav className="w-full bg-white sticky top-0 z-50">
+    <nav className="w-full bg-white sticky top-0 z-50 h-18">
       <div className="max-w-8xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div className="flex justify-between items-center py-4">
+        <div className="flex justify-between items-center py-0">
           {/* Logo */}
           <div className="flex items-center space-x-2">
             <img src={NationalLogo} alt="National Logo" className="h-18 w-18" />
@@ -23,13 +23,13 @@ export default function Navbar() {
           {/* Desktop nav links */}
           <div className="hidden md:flex space-x-25">
             {navLinks.map((link) => (
-                <Link
-                    key={link}
-                    to={link === "Home" ? "/" : `/${link.toLowerCase()}`}
-                    className="text-gray-800 font-medium text-lg hover:text-orange-600 transition font-poppins"
-                >
-                    {link}
-                </Link>
+              <Link
+                key={link}
+                to={link === "Home" ? "/" : `/${link.toLowerCase()}`}
+                className="text-gray-800 font-medium text-lg hover:text-orange-600 transition font-poppins"
+              >
+                {link}
+              </Link>
             ))}
           </div>
 
@@ -65,13 +65,13 @@ export default function Navbar() {
         {menuOpen && (
           <div className="md:hidden flex flex-col space-y-4 pb-4">
             {navLinks.map((link) => (
-                <Link
-                    key={link}
-                    to={link === "Home" ? "/" : `/${link.toLowerCase()}`}
-                    className="text-gray-800 font-medium text-lg hover:text-orange-600 transition font-poppins"
-                >
-                    {link}
-                </Link>
+              <Link
+                key={link}
+                to={link === "Home" ? "/" : `/${link.toLowerCase()}`}
+                className="text-gray-800 font-medium text-lg hover:text-orange-600 transition font-poppins"
+              >
+                {link}
+              </Link>
             ))}
             <button className="bg-orange-200 text-orange-600 px-4 py-2 rounded-md font-medium hover:bg-orange-100 transition font-poppins">
               Log In
