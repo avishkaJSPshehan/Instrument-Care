d66e092305dd18f7135ca3a368cdc0fe2b4577a8
Avishka Shehan <152954657+avishkaJSPshehan@users.noreply.github.com>
2025-08-11 16:00:01 +0530

    Add Drop down for Instrument and laboratory category selection

--- Instrument-Care-Front-End/src/Components/Technician/ProfileFormLeft.jsx
+++ Instrument-Care-Front-End/src/Components/Technician/ProfileFormLeft.jsx
@@ -3,60 +3,144 @@ import React from "react";
 export default function ProfileFormLeft({ formData, handleChange }) {
   return (
     <div className="flex flex-col gap-3">
-      <label>
-        Full Name *
+      <h5 className="font-bold">Affiliation</h5>
+      <label className="pl-4">
+        Institute Name 
         <input
           type="text"
-          name="fullName"
+          name="fullNames"
           value={formData.fullName}
           onChange={handleChange}
-          className="border rounded p-2 w-full"
+          className="border rounded p-1 w-full"
+        />
+      </label>
+      <label className="pl-4">
+        Designation
+        <input
+          type="text"
+          name="fullNames"
+          value={formData.fullName}
+          onChange={handleChange}
+          className="border rounded p-1 w-full"
         />
       </label>
 
-      <label>
-        Email Address *
+      <h5 className="font-bold">Details of the Supervisor</h5>
+
+      <label className="pl-4">
+        Name 
         <input
           type="email"
-          name="email"
+          name="emails"
           value={formData.email}
           onChange={handleChange}
-          className="border rounded p-2 w-full"
-          placeholder="example@example.com"
+          className="border rounded p-1 w-full"
         />
       </label>
 
-      <label>
-        Physical Address *
+      <label className="pl-4">
+        Designation 
         <input
-          type="text"
-          name="address"
-          value={formData.address}
+          type="email"
+          name="emails"
+          value={formData.email}
           onChange={handleChange}
-          className="border rounded p-2 w-full"
+          className="border rounded p-1 w-full"
         />
       </label>
 
-      <label>
-        Personal Number *
+      <label className="pl-4">
+        Email 
         <input
-          type="text"
-          name="personalNumber"
-          value={formData.personalNumber}
+          type="email"
+          name="emails"
+          value={formData.email}
           onChange={handleChange}
-          className="border rounded p-2 w-full"
+          className="border rounded p-1 w-full"
         />
       </label>
 
-      <label>
-        Profile Bio *
-        <textarea
-          name="bio"
-          value={formData.bio}
+      <label className="pl-4">
+        Contract No 
+        <input
+          type="email"
+          name="emails"
+          value={formData.email}
           onChange={handleChange}
-          className="border rounded p-2 w-full"
+          className="border rounded p-1 w-full"
         />
       </label>
+
+    <h5 className="font-bold">Techinical Expertise</h5>
+
+      <label className="pl-4">
+        Laboratory Category
+        <select
+          name="laboratory_category"
+          value={formData.contractNo}
+          onChange={handleChange}
+          className="border rounded p-2 w-full"
+        >
+          <option value="">Select Laboratory Category</option>
+          <option value="1">Chemical Testing</option>
+          <option value="2">Biological Testing</option>
+          <option value="3">Physical & Mechanical Testing</option>
+          <option value="4">Calibration Service</option>
+          <option value="5">Biochemistry, Chemical Pathology, Clinical Pathology</option>
+          <option value="6">Hematology and Immuno Hematology</option>
+          <option value="7">Microbiology and Serology</option>
+          <option value="8">Histopathology/Cytopathology</option>
+          <option value="9">Immunology</option>
+          <option value="10">Molecular Biology</option>
+          <option value="11">Pharmacology</option>
+          <option value="12">Andrology Clinical</option>
+          <option value="13">Nuclear Medicine</option>
+          <option value="14">Embryology</option>
+        </select>
+      </label>
+
+      <label className="pl-4">
+        Instrument Category
+        <select
+          name="instrument_category"
+          value={formData.contractNo}
+          onChange={handleChange}
+          className="border rounded p-2 w-full"
+        >
+          <option value="">Select Instrument Category</option>
+          <option value="1">Analiytical Balance</option>
+          <option value="2">Microscope</option>
+          <option value="3">Centrifuger</option>
+          <option value="4">Gas Chromatograph & GC/MS</option>
+          <option value="5">High Performance Liquid Chromatograph (HPLC)</option>
+          <option value="6"></option>
+          <option value="7"></option>
+          <option value="8"></option>
+          <option value="9"></option>
+          <option value="10"></option>
+          <option value="11"></option>
+          <option value="12"></option>
+          <option value="13"></option>
+          <option value="15"></option>
+          <option value="16"></option>
+          <option value="17"></option>
+          <option value="18"></option>
+          <option value="19"></option>
+          <option value="20"></option>
+          <option value="21"></option>
+          <option value="22"></option>
+          <option value="23"></option>
+          <option value="24"></option>
+          <option value="25"></option>
+          <option value="26"></option>
+          <option value="27"></option>
+          <option value="28"></option>
+          <option value="29"></option>
+          <option value="30"></option>
+        </select>
+      </label>
+
+
     </div>
   );
 }
--- Instrument-Care-Front-End/src/Components/Technician/ProfileImageUpload.jsx
+++ Instrument-Care-Front-End/src/Components/Technician/ProfileImageUpload.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import profileImage from '../../assets/images/profile-image.jpeg';
 
-export default function ProfileImageUpload() {
+export default function ProfileImageUpload({ formData, handleChange }) {
   return (
     <div className="flex flex-col items-center p-4">
       {/* Profile Image Placeholder */}
@@ -12,11 +12,82 @@ export default function ProfileImageUpload() {
             alt="Profile"
             className="h-28 w-28 rounded-full object-cover cursor-pointer border border-gray-300 hover:scale-105 transition-transform"
         />
+        
       </div>
+      
       <p className="text-sm text-gray-500">Brows Image From your computer</p>
       <button className="mt-2 bg-cyan-600 text-white px-4 py-2 rounded">
         Upload Image
       </button>
+      
+      <div className="flex flex-col gap-3">
+        <h5 className="font-bold">Personal Details</h5>
+
+      <label>
+        Full Name *
+        <input
+          type="text"
+          name="fullName"
+          value={formData.fullName}
+          onChange={handleChange}
+          className="border rounded p-1 w-full font-normal"
+        />
+      </label>
+
+      <label>
+        National Identity Card Number *
+        <input
+          type="text"
+          name="nic"
+          value={formData.nic}
+          onChange={handleChange}
+          className="border rounded p-1 w-full font-normal"
+        />
+      </label>
+
+      <label>
+        Email Address *
+        <input
+          type="email"
+          name="email"
+          value={formData.email}
+          onChange={handleChange}
+          className="border rounded p-1 w-full font-normal"
+        />
+      </label>
+
+      <label>
+        Physical Address *
+        <input
+          type="text"
+          name="address"
+          value={formData.address}
+          onChange={handleChange}
+          className="border rounded p-1 w-full font-normal"
+        />
+      </label>
+
+      <label>
+        Mobile Number *
+        <input
+          type="text"
+          name="personalNumber"
+          value={formData.personalNumber}
+          onChange={handleChange}
+          className="border rounded p-1 w-full font-normal"
+        />
+      </label>
+
+      <label>
+        Profile Bio *
+        <textarea
+          name="bio"
+          value={formData.bio}
+          onChange={handleChange}
+          className="border rounded p-5 w-full font-normal"
+        />
+      </label>
+    </div>
     </div>
   );
 }
