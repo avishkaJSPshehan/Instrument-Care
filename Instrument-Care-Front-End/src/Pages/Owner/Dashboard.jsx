f385b90239801849d9fc6cf2b8c340dbd9105083
Avishka Shehan <152954657+avishkaJSPshehan@users.noreply.github.com>
2025-08-15 14:31:36 +0530

    Create hero section for the user technician finding page

--- Instrument-Care-Front-End/src/Pages/Owner/Dashboard.jsx
+++ Instrument-Care-Front-End/src/Pages/Owner/Dashboard.jsx
@@ -3,13 +3,14 @@ import Navbar from '../../Components/owner/Navbar'
 import Footer from '../../Components/Common/Footer'
 import TechniciansCard from '../../Components/owner/TechniciansCard'
 import SearchBar from '../../Components/owner/SearchBar'
+import TechnicianHeader from '../../Components/owner/Technician-Hero-Section'
 
 
 export default function User_Dashboard() {
   return (
     <>
       <Navbar />
-        <SearchBar />
+        <TechnicianHeader />
         <TechniciansCard/>
       <Footer />
     </>
